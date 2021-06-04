d96e9923ffb7f8ebc05dd8a950586a5e445e8f66
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-06-04 18:26:40 -0400

    Update setup for .py files

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars.py
+
+
+app = flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/phone_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    print("")
+    #listings = mongo.db.listings.find_one()
+    #listings_data = scrape_mars.py.scrape()
+
+if __name__ == "__main__":
+    app.run(debug=True)
