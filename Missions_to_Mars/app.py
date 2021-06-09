9bb1ce14a1f6d9c55961b69880ddccc8dd2cf79d
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-06-08 23:51:04 -0400

    Work on app and scrape functionality

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -2,27 +2,26 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-
 app = Flask(__name__)
 
 # app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-# mongo = PyMongo(app)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
 
 @app.route("/")
 def home():
+    destination_data = mongo.db.collectionvariables_dict.find_one()
     
-   
-
     # Return template and data
-    return render_template("index.html")
+    return render_template("index.html", mars=destination_data ) 
+    
 
 @app.route("/scrape")
 def scraper():
-
-# listings = mongo.db.listings
-    scraped_data = scrape_mars.scrape()    
-# like listings_data = scrape_phone.scrape()
-#     listings.update({}, listings_data, upsert=True)
+   
+    variables_dict = scrape_mars.scrape()    
+    mongo.db.collection.update({}, variables_dict, upsert=True)
+    
     return redirect("/", code=302)
 
 if __name__ == "__main__":
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from splinter import Browser
-from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -8,7 +7,7 @@ def scrape():
     # browser = init_browser()
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
-
+    
     # Identify the website url to be visited for news, and scrape for first news title and paragraph
     browser.visit('https://redplanetscience.com/')
     news_title = browser.find_by_css('div.content_title').text
@@ -18,32 +17,37 @@ def scrape():
     browser.visit('https://spaceimages-mars.com/')
 
     # Click the button "FULL IMAGE" that displays the image and save the image url to a variable
-    browser.find_link_by_partial_text('FULL IMAGE').click()
+    browser.links.find_by_partial_text('FULL IMAGE').click()
     featured_image_url = browser.find_by_css('img.fancybox-image')['src']
 
     # Identify the website url to be visited for facts and use pd.read_html to grab the html
     mars_facts = pd.read_html('https://galaxyfacts-mars.com/')[0].to_html()
 
     # Identify the website url to be visited for the hemispheres and create a list for the four urls
+    # Identify the website url to be visited
     browser.visit('https://marshemispheres.com/')
+
+    # Create a dictionary to store the results
     hemispheres_image_urls = []
     # Set up a for loop that will happen four times (one for each hemisphere link)
-    for x in range(1, 5):
-
-    # Click the links that display the image 
-        browser.find_link_by_partial_text('Enhanced').click()
+    for x in range(0, 4):
+        
+        # Click the links that lead to the full image 
+        browser.links.find_by_partial_text('Enhanced')[x].click()
+        
+        # Find and save all the titles and urls to the full resolution images (Sample links)
         hemisphere_name = browser.find_by_css('h2.title').text
-        img_url = browser.find_by_css('img.thumb')['src']
+        img_url = browser.links.find_by_text('Sample')['href']
         hemispheres_dict = {"title":hemisphere_name, 'img_url':img_url}
         hemispheres_image_urls.append(hemispheres_dict)
-        browser.back()
-
+        browser.back()   
+        
     variables_dict = {
-        'Latest Mars News':news_title,
-        'Latest News Paragraph':news_p,
-        'Featured Mars Image':featured_image_url,
-        'Mars Facts':mars_facts,
-        'Mars Hemispheres':hemispheres_image_urls
+        'news':news_title,
+        'paragraph':news_p,
+        'featured':featured_image_url,
+        'facts':mars_facts,
+        'hemispheres':hemispheres_image_urls
     }  
     # Quit the browser
     browser.quit()
